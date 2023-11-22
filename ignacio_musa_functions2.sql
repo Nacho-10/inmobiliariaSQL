332ba13ce6c04ecf2e06b7eaecd85a8921244f1b
Nacho-10 <ignaciomusa26@gmail.com>
2023-11-22 17:44:29 -0300

    function 2

---
+++ ignacio_musa_functions2.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION ObtenerPrecioPromedioPorTipo(tipoPropiedad VARCHAR(50))
+RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+BEGIN
+    DECLARE precioPromedio DECIMAL(10, 2);
+
+    SELECT AVG(Precio) INTO precioPromedio
+    FROM Propiedades
+    WHERE TipoDePropiedad = tipoPropiedad;
+
+    RETURN precioPromedio;
+END;
