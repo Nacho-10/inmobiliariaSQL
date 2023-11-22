21dee0807d9cfe0720d4c25cce759d803ecdc392
Nacho-10 <ignaciomusa26@gmail.com>
2023-11-22 17:41:31 -0300

    function 1

---
+++ ignacio_musa_functions1.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION ContarReservasPorPropiedad(propiedadID INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE numReservas INT;
+
+    SELECT COUNT(*) INTO numReservas
+    FROM Reservas
+    WHERE PropiedadID = propiedadID;
+
+    RETURN numReservas;
+END;
